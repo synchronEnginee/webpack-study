08f03a09457f76fe6dfc2979b945825a399eba21
synchronEnginee <te2.9niku.41cd@gmail.com>
2021-12-05 18:19:45 +0900

    Don't use file-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 module.exports = {
     mode:'development',
     devtool: false,
-    entry: { app: './src/index.js' },
+    entry: path.join(__dirname, '/src','index.js'),
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'app.js'
+    },
     module: {
         rules: [
             {
@@ -12,6 +18,13 @@ module.exports = {
                     'postcss-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(jpg|png|jpe?g|gif|svg|woff2?|ttf|eot)$/,
+                generator: {
+                        filename: 'images/[name][ext][query]',
+                }, 
+                type: 'asset/resource'
             }
         ]
     }
